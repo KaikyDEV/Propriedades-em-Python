784dea405d2d7129aa1f5d8eb00145ad339d5211
Kaiky - Santos <126119808+KaikyDEV@users.noreply.github.com>
2024-07-04 21:31:16 -0300

    Add files via upload

---
+++ propriedades.py
@@ -0,0 +1,28 @@
+# Exemplo de propriedades
+
+class Foo:
+    def __init__(self, x=None):
+        self._x = x
+
+
+    @property
+    def x(self):
+        return self._x or 0
+
+    @x.setter
+    def x(self, value):
+        _x = self._x or 0
+        _value = value or 0
+        self._x = _x + value
+
+    @x.deleter
+    def x(self):
+        self._x = -1
+
+
+foo = Foo()
+print(foo.x)
+foo.x = 10
+print(foo.x)
+del foo.x
+print(foo.x)
